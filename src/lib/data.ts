a0c3ad30353f39794fdacc4b13c0ddfff08c4178
Lui24 <gnu936@gmail.com>
2024-04-22 18:28:51 -0500

    full task

---
+++ src/lib/data.ts
@@ -0,0 +1,21 @@
+export interface Song {
+  image: string;
+  title: string;
+  artist: string;
+  song: string;
+}
+
+export const songs: Song[] = [
+  {
+    "image": "img/cover-1.webp",
+    "title": "Lost in the City Lights",
+    "artist": "Cosmo Sheldrake",
+    "song": "audios/lost-in-city-lights-145038.mp3"
+  },
+  {
+    "image": "img/cover-2.webp",
+    "title": "Forest Lullaby",
+    "artist": "Lesfm",
+    "song": "audios/forest-lullaby-110624.mp3"
+  }
+];
